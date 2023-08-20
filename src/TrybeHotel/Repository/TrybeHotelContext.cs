b399445c73fef722d6188a5ef54a6620e417f89b
liciniopsj <licinio.psj@gmail.com>
2023-08-20 16:47:33 -0300

    Dbset and Context

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,13 +4,22 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) {
+    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
+    {
         Seeder.SeedUserAdmin(this);
     }
     public TrybeHotelContext() { }
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
+    public DbSet<City> Cities { get; set; } = null!;
+    public DbSet<Hotel> Hotels { get; set; } = null!;
+    public DbSet<Room> Rooms { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Booking> Bookings { get; set; } = null!;
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {}
+    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 
 }
