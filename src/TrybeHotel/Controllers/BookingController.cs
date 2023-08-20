65ec7fd19bdbb125a0e835c7a7da755b7bcf036a
liciniopsj <licinio.psj@gmail.com>
2023-08-20 18:36:55 -0300

    Post Booking

--- src/TrybeHotel/Controllers/BookingController.cs
+++ src/TrybeHotel/Controllers/BookingController.cs
@@ -11,7 +11,7 @@ namespace TrybeHotel.Controllers
 {
     [ApiController]
     [Route("booking")]
-  
+
     public class BookingController : Controller
     {
         private readonly IBookingRepository _repository;
@@ -21,14 +21,27 @@ namespace TrybeHotel.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add([FromBody] BookingDtoInsert bookingInsert){
-            throw new NotImplementedException();
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Authorize(Policy = "Client")]
+        public IActionResult Add([FromBody] BookingDtoInsert bookingInsert)
+        {
+            try
+            {
+                var userEmail = User.FindFirstValue(ClaimTypes.Email);
+                var NewBooking = _repository.Add(bookingInsert, userEmail);
+                return Created(string.Empty, NewBooking);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(new { message = err.Message });
+            }
         }
 
 
         [HttpGet("{Bookingid}")]
-        public IActionResult GetBooking(int Bookingid){
-           throw new NotImplementedException();
+        public IActionResult GetBooking(int Bookingid)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- src/TrybeHotel/Repository/BookingRepository.cs
+++ src/TrybeHotel/Repository/BookingRepository.cs
@@ -1,5 +1,6 @@
 using TrybeHotel.Models;
 using TrybeHotel.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace TrybeHotel.Repository
 {
@@ -13,7 +14,51 @@ namespace TrybeHotel.Repository
 
         public BookingResponse Add(BookingDtoInsert booking, string email)
         {
-            throw new NotImplementedException();
+            var room = _context.Rooms
+                .Include(room => room.Hotel)
+                .ThenInclude(hotel => hotel.City)
+                .FirstOrDefault(room => room.RoomId == booking.RoomId);
+
+            if (room == null || booking.GuestQuant > room.Capacity)
+            {
+                throw new Exception("Guest quantity over room capacity");
+            }
+
+            var newBooking = new Booking
+            {
+                CheckIn = booking.CheckIn,
+                CheckOut = booking.CheckOut,
+                GuestQuant = booking.GuestQuant,
+                Room = room
+            };
+
+            _context.Bookings.Add(newBooking);
+            _context.SaveChanges();
+
+            return new BookingResponse
+            {
+                BookingId = newBooking.BookingId,
+                CheckIn = newBooking.CheckIn,
+                CheckOut = newBooking.CheckOut,
+                GuestQuant = newBooking.GuestQuant,
+                Room = new RoomDto
+                {
+                    RoomId = room.RoomId,
+                    Name = room.Name,
+                    Capacity = room.Capacity,
+                    Image = room.Image,
+                    Hotel = new HotelDto
+                    {
+                        HotelId = room.Hotel.HotelId,
+                        Name = room.Hotel.Name,
+                        Address = room.Hotel.Address,
+                        CityId = room.Hotel.CityId,
+                        CityName = room.Hotel.City.Name
+                    }
+                }
+            };
+
+
         }
 
         public BookingResponse GetBooking(int bookingId, string email)
